a5ca0a09eb69866b29b92badc5cff0dc47c89357
Adam M. Rahman <mrahmanadam@gmail.com>
2019-07-20 11:25:47 -0400

    Adjusted indexing of payload

--- core/designers.py
+++ core/designers.py
@@ -8,37 +8,34 @@ client = gspread.authorize(creds)
 
 sheet = client.open('2s_designerbase').sheet1
 
+def get_designer_records():
+    return sheet.get_all_records()
+
 def store_designer(args):
     '''
     Parses Typeform response into a Designer schema, 
     then stores it in the Google Sheet 
     '''
     answer_list = ast.literal_eval(args["form_response"])["answers"]
-    print(answer_list[2]['text'])
     # If the user agrees with the disclaimer
     # we collect and store their responses in the Google Sheet
-    if answer_list[0]['boolean']:
-        first_name = answer_list[1]['text']
-        last_name = answer_list[2]['text']
-        email = answer_list[3]['email']
-        class_year = answer_list[4]['number']
-        design_choices = str(answer_list[5]['choices']['labels']).strip('[\']"')
-        specialized_choices = str(answer_list[6]['choices']['labels']).strip('[\']"')
-        possible_assistant = str(answer_list[7]['boolean'])
-        experience = answer_list[8]['text']
-        more_info = answer_list[9]['text']
-        row = [first_name, last_name, email, class_year, design_choices, 
+    first_name = answer_list[0]['text']
+    last_name = answer_list[1]['text']
+    email = answer_list[2]['email']
+    class_year = answer_list[3]['number']
+    design_choices = str(answer_list[4]['choices']['labels']).strip('[\']"')
+    specialized_choices = str(answer_list[5]['choices']['labels']).strip('[\']"')
+    possible_assistant = str(answer_list[6]['boolean'])
+    experience = answer_list[7]['text']
+    more_info = answer_list[8]['text']
+    row = [first_name, last_name, email, class_year, design_choices, 
                specialized_choices, possible_assistant, experience, more_info ]
-        if len(sheet.get_all_records()) == 0:
+    if len(sheet.get_all_records()) == 0:
             index = 2
-        else:
-            index = len(sheet.get_all_records()) + 1
-        sheet.insert_row(row, index)
-
-        return row
-
+    else:
+        index = len(sheet.get_all_records()) + 1
+    sheet.insert_row(row, index)
+    return row
     else:
         return None
 
-def get_designer_records():
-    return sheet.get_all_records()
